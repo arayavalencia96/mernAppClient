9fefbabc159af8b8c4f8151c33f5047040294b9a
Axel <arayavalencia96@gmail.com>
2022-08-24 16:50:19 -0300

    se le agrego la s a HTTP://.. de tasks.api.js

--- src/api/tasks.api.js
+++ src/api/tasks.api.js
@@ -1,21 +1,21 @@
 import axios from 'axios';
 
 export const getTasksRequest = async () =>
-    await axios.get('http://mernappfullstack-job.herokuapp.com/tasks');
+    await axios.get('https://mernappfullstack-job.herokuapp.com/tasks');
 
 export const createTaskRequest = async (task) =>
-    await axios.post('http://mernappfullstack-job.herokuapp.com/tasks', task);
+    await axios.post('https://mernappfullstack-job.herokuapp.com/tasks', task);
 
 export const deleteTaskRequest = async (id) =>
-    await axios.delete(`http://mernappfullstack-job.herokuapp.com/tasks/${id}`);
+    await axios.delete(`https://mernappfullstack-job.herokuapp.com/tasks/${id}`);
 
 export const getTaskRequest = async (id) =>
-    await axios.get(`http://mernappfullstack-job.herokuapp.com/tasks/${id}`);
+    await axios.get(`https://mernappfullstack-job.herokuapp.com/tasks/${id}`);
 
 export const updateTaskRequest = async (id, newFields) =>
-    await axios.put(`http://mernappfullstack-job.herokuapp.com/tasks/${id}`, newFields);
+    await axios.put(`https://mernappfullstack-job.herokuapp.com/tasks/${id}`, newFields);
 
 export const toggleTaskDoneRequest = async (id, done) =>
-    await axios.put(`http://mernappfullstack-job.herokuapp.com/tasks/${id}`,{
+    await axios.put(`https://mernappfullstack-job.herokuapp.com/tasks/${id}`,{
         done,
     });
