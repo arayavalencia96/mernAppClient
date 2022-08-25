69b9d8a177d283d6cba1b1709d89e4e2a87dc06a
Axel <arayavalencia96@gmail.com>
2022-08-24 22:19:02 -0300

    2condUpdate NotFound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,15 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFaceDizzy } from "@fortawesome/free-solid-svg-icons";
+
 function NotFound() {
   return (
-    <div>
-      <h1 className="d-flex justify-content-center">
-        La página a la que querés acceder no se encuentra a o no existe.
-      </h1>
+    <div className='font-size: large'>
+      <h3 className="d-flex justify-content-center">
+        La página a la que querés acceder no se encuentra o no existe.
+      </h3>
+      <div className="d-flex justify-content-center font-size: large">
+        <FontAwesomeIcon size='lg' icon={faFaceDizzy} />
+      </div>
     </div>
   );
 }
